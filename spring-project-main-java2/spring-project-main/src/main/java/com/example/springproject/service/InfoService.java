0cfc60fea4e277736e91bdfef5ef7175fdb55a9c
dengsh12 <62536982+dengsh12@users.noreply.github.com>
2022-01-19 21:18:42 +0800

    Update InfoService.java

--- spring-project-main-java2/spring-project-main/src/main/java/com/example/springproject/service/InfoService.java
+++ spring-project-main-java2/spring-project-main/src/main/java/com/example/springproject/service/InfoService.java
@@ -1,7 +1,9 @@
 package com.example.springproject.service;
 
 import com.example.springproject.domain.Data;
+import com.example.springproject.domain.FileData;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 //import java.util.Date;
@@ -10,11 +12,13 @@ public interface InfoService {
     public void setType(String type);
     public ArrayList<Data> queryAll();
     public ArrayList<Data> search(String columnName,String value);
-    public ArrayList<Data> sort(String columnName, String value);
-    public void save(String filePath);
-    public ArrayList<Data> sort(ArrayList<Data> list, String columnName, String upOrDown);
-    public ArrayList<Data> search(ArrayList<Data> list, String columnName, String value);
-    public void save(String filePath, ArrayList<Data> list);
+    public ArrayList<Data> sort(String columnName, String upOrDown);
+    public ArrayList<Data> sort(ArrayList<Data> list,String columnName, String upOrDown);// up:from small to big
+    public ArrayList<Data> search(ArrayList<Data> list,String columnName,String value);
+    public void save();
+    public void save(String filePath,ArrayList<Data> list);
     public ArrayList<Data> getLastOneByLocation();
     public String[] getRandomHex();
+    public ArrayList<ArrayList<String>> getOnOneMonth(LocalDate date);
+//    public ArrayList<Data>
 }
